d884dc7cd3264f784a137f9329268d961be20526
Oxyno-zeta <havrileckalexandre@gmail.com>
2015-12-07 23:20:40 +0100

    [Shared] Logger fixed

--- src/shared/logger.js
+++ src/shared/logger.js
@@ -7,16 +7,66 @@
 // Require
 var log4js = require('log4js');
 // Load configuration
-log4js.configure('log4js_Configuration.json', {});
+log4js.configure('log4jsConf.json', {});
 // Get logger
 var logger = log4js.getLogger('cheese');
 
 // Export
 module.exports = {
-    info: logger.info,
-    trace: logger.trace,
-    debug: logger.debug,
-    warn: logger.warn,
-    error: logger.error,
-    fatal: logger.fatal
+    info: info,
+    trace: trace,
+    debug: debug,
+    warn: warn,
+    error: error,
+    fatal: fatal
 };
+
+// Functions
+/**
+ * Fatal log level
+ * @param text {*}
+ */
+function fatal(text){
+    logger.fatal(text);
+}
+
+/**
+ * Error log level
+ * @param text {*}
+ */
+function error(text){
+    logger.error(text);
+}
+
+/**
+ * Warn log level
+ * @param text {*}
+ */
+function warn(text){
+    logger.warn(text);
+}
+
+/**
+ * Debug log level
+ * @param text {*}
+ */
+function debug(text){
+    logger.debug(text);
+}
+
+/**
+ * Trace log level
+ * @param text {*}
+ */
+function trace(text){
+    logger.trace(text);
+}
+
+/**
+ * Info log level
+ * @param text {*}
+ */
+function info(text){
+    logger.info(text);
+}
+
