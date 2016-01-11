acbd1dde83ace7ce1d8f0ed3fb6320587e0eb4b2
Oxyno-zeta <havrileckalexandre@gmail.com>
2016-01-11 18:23:19 +0100

    [Gulp] New bump task

--- gulpfile.js
+++ gulpfile.js
@@ -54,19 +54,19 @@ gulp.task('default', function(cb){
 gulp.task('bump', ['bump:patch']);
 
 gulp.task('bump:major', function(){
-    return gulp.src('package.json')
+    return gulp.src(['package.json', 'bower.json'])
         .pipe(bump({type:'major'}))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('bump:minor', function(){
-    return gulp.src('package.json')
+    return gulp.src(['package.json', 'bower.json'])
         .pipe(bump({type:'minor'}))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('bump:patch', function(){
-    return gulp.src('package.json')
+    return gulp.src(['package.json', 'bower.json'])
         .pipe(bump({type:'patch'}))
         .pipe(gulp.dest('./'));
 });
