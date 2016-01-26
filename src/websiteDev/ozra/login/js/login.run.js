9d28049119b36561990258a6e6dfb932a8da25df
Oxyno-zeta <havrileckalexandre@gmail.com>
2016-01-26 22:05:18 +0100

    [Site][Login] Login auto

---
+++ src/websiteDev/ozra/login/js/login.run.js
@@ -0,0 +1,42 @@
+/*
+ * Author: Alexandre Havrileck (Oxyno-zeta) 
+ * Date: 25/01/16
+ * Licence: See Readme
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('ozra.login')
+        .run(runRouteErrors);
+
+        /** @ngInject */
+        function runRouteErrors ($rootScope, $state, dataCacheService) {
+            $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error){
+                if ( !(_.isUndefined(error) && _.isNull(error)) && _.isEqual(error.status, 403)){
+                    event.preventDefault(); // Stop transition
+                    // Remove data
+                    dataCacheService.removeToken();
+                    dataCacheService.removeUserId();
+                    // Go to state
+                    $state.go('login');
+                }
+            });
+
+            $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, error){
+                var token = dataCacheService.token;
+                if (!_.isEqual(toState.name, 'login') && (_.isUndefined(token) || _.isNull(token)) ){
+                    event.preventDefault(); // Stop transition
+                    $state.go('login');
+                    return;
+                }
+
+                if (_.isEqual(toState.name, 'login') && !(_.isUndefined(token) || _.isNull(token)) ){
+                    event.preventDefault();
+                    $state.go('ozra');
+                }
+            });
+        }
+
+})();
