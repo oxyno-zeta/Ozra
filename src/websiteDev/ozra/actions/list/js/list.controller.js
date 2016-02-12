1962e9d97f9fd9d6346868cb140fdc439ba68533
Oxyno-zeta <havrileckalexandre@gmail.com>
2016-02-12 18:13:20 +0100

    [Actions][List] Fix design (name too long possibility), run action, add action

--- src/websiteDev/ozra/actions/list/js/list.controller.js
+++ src/websiteDev/ozra/actions/list/js/list.controller.js
@@ -12,12 +12,59 @@
         .controller('ActionsListController', ActionsListController);
 
     /* @ngInject */
-    function ActionsListController(actions) {
+    function ActionsListController($mdToast, $mdDialog, actionService, actions) {
         var vm = this;
         // Variables
         vm.actions = actions;
+        // Functions
+        vm.run = run;
 
         ////////////////
+
+        /**
+         * Run action
+         * @param actionId
+         */
+        function run(actionId){
+            actionService.runFromId(actionId).then(function(response){
+                // Content text : default error
+                var contentText = 'Error launching action !';
+                if (_.isEqual(response.result.code, 0)){
+                    contentText = 'Action successfully launched !';
+                }
+
+                // Create toast
+                var toast = $mdToast.simple()
+                    .textContent(contentText)
+                    .position('top right')
+                    .action('See log ?')
+                    .hideDelay(1500);
+
+                // Show toast
+                $mdToast.show(toast).then(function(_response){
+                    if (_.isEqual(_response, 'ok')){
+                        $mdDialog.show({
+                            controller: 'ActionLogDialog',
+                            controllerAs: 'actionLogDialog',
+                            templateUrl: 'ozra/dialogs/actionLogDialog/actionLogDialog.html',
+                            locals: {
+                                result: response.result
+                            },
+                            clickOutsideToClose:true
+                        });
+                    }
+                });
+            }, function(err){
+                if (err.hasOwnProperty('reason')) {
+                    var toast = $mdToast.simple()
+                        .textContent(err.reason)
+                        .position('top right')
+                        .hideDelay(1500);
+                    // Show toast
+                    $mdToast.show(toast);
+                }
+            });
+        }
 
     }
 
